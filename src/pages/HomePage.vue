218e6275e28877a6f2b3da840ddb47e3cb5d8ae9
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-26 10:55:02 -0700

    interpolation variable

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -10,6 +10,7 @@
         <img class="img-fluid"
           src="https://images.unsplash.com/photo-1479090793912-eb9929f4fdb2?q=80&w=968&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="">
+        <h2 class="mt-2">Cheese: {{ cheese }}</h2>
       </div>
     </section>
   </div>
@@ -18,8 +19,11 @@
 <script>
 export default {
   setup() {
-    return {
+    // NOTE private
+    let cheese = 0
 
+    return {
+      cheese
     }
   }
 }
