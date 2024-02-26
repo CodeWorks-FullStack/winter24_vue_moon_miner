57ad218508fd896459cc9661e9fb13642f39b466
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-26 11:09:12 -0700

    reactivity with ref

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -7,23 +7,33 @@
         </div>
       </div>
       <div class="col-12 text-center">
-        <img class="img-fluid"
+        <img @click="mineCheese()" class="img-fluid"
           src="https://images.unsplash.com/photo-1479090793912-eb9929f4fdb2?q=80&w=968&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="">
-        <h2 class="mt-2">Cheese: {{ cheese }}</h2>
+        <h2 class="mt-2">Cheese: 🧀{{ cheese }}</h2>
       </div>
     </section>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
   setup() {
     // NOTE private
-    let cheese = 0
+    const cheese = ref(0)
+
 
     return {
-      cheese
+      // NOTE public
+      cheese,
+
+      mineCheese() {
+        cheese.value++
+        console.log('cheese object', cheese);
+        console.log('cheese value', cheese.value);
+      }
     }
   }
 }
